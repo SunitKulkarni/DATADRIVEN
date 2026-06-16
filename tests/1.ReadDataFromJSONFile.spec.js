56a931d866450db2f777d9db0339abe31bf34fd6
sunitkulkarni <sunitkulkarni@hotmail.com>
2026-06-16 17:43:55 +0530

    updated

--- tests/1.ReadDataFromJSONFile.spec.js
+++ tests/1.ReadDataFromJSONFile.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import global from "../config/global.json";
 
 
-test('REad Data From JSON File ', async ({ page }) => {
+test('Read Data From JSON File ', async ({ page }) => {
 console.log (global.url);
 console.log (global.username);
 console.log (global.password);
